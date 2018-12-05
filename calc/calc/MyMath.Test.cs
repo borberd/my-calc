bc9ac89aa1c2cca9fa8058c995ade29142751b17
borberd <borberd@gmail.com>
2018-12-05 16:54:38 +0000

    Add files via upload

---
+++ calc/calc/MyMath.Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace calc
+{
+    class MyMathTest
+    {
+        [TestFixture]
+
+        class Test_Add
+        {
+            [TestCase]
+            public void TestAddPositives()
+
+            {
+                MyMath math = new MyMath();
+                Assert.AreEqual(30, math.Add(10 , 20));
+            }
+            [TestCase]
+            public void TestAddNegative()
+
+            {
+                MyMath math = new MyMath();
+                Assert.Greater(55, math.Add(10, 20));
+            }
+            [TestCase]
+            public void TestAddCore()
+            {
+                MyMath math = new MyMath();
+                Assert.Greater(math.Add(30, 10), 30);
+
+
+            }
+
+
+        }
+    }
+}
